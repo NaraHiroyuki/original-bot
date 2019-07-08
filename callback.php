81a317544b78fd9c4cb5ea36cb904ccf9278ceb8
NaraHiroyuki <hiroyuki0818yakyu@icloud.com>
2019-07-08 20:49:29 +0900

    カルーセル機能を修正

--- callback.php
+++ callback.php
@@ -194,28 +194,29 @@ if(!empty($areaID)){
 }
 
 if($message_text == "カルーセル"){
-    $return_message_text  = [
-        "type" => "template",
-        "altText" => "カルーセル",
-        "template" => [
-            "type" => "carousel",
-            "columns" => [
-              "text" => "カルーセル",
-              "title" => "カルーセル",
-              "actions" => 
-                [
-                  'type' => 'postback', 
-                  'label' => 'webhookにpost送信', 
-                  'data' => 'value' 
-                ], 
-                [
-                  "type" => "uri",
-                  "label" => "福岡の情報",
-                  "uri" => "https://www.google.com/search?q=%E7%A6%8F%E5%B2%A1+%E8%A6%B3%E5%85%89&rlz=1C5CHFA_enJP839JP839&oq=%E7%A6%8F%E5%B2%A1%E3%80%80%E8%A6%B3%E5%85%89&aqs=chrome..69i57j35i39j0l4.6508j0j8&sourceid=chrome&ie=UTF-8"
-                ]
+    [
+     "type" => "template",
+     "altText" => "カルーセル",
+     $return_message_text = [
+        "type" => "carousel",
+        "columns" => [
+            "text" => "カルーセル",
+            "title" => "カルーセル",
+            "action" =>  
+            [
+                'type' => 'postback', 
+                'label' => 'webhookにpost送信', 
+                'data' => 'value' 
+            ], 
+            [
+                "type" => "uri",
+                "label" => "福岡の情報",
+                "uri" => "https://www.google.com/search?q=%E7%A6%8F%E5%B2%A1+%E8%A6%B3%E5%85%89&rlz=1C5CHFA_enJP839JP839&oq=%E7%A6%8F%E5%B2%A1%E3%80%80%E8%A6%B3%E5%85%89&aqs=chrome..69i57j35i39j0l4.6508j0j8&sourceid=chrome&ie=UTF-8"
             ]
         ]
+     ]
     ];
+    
     send_carousel($accessToken, $replyToken,$messageData, $return_message_text);
 } else {
     //返信実行
@@ -259,7 +260,11 @@ function send_carousel($accessToken, $replyToken,$messageData, $return_message_t
      
 
     //レスポンスフォーマット
-    $response_format_text = [$return_message_text];
+    $response_format_text = [
+        "type" => "template",
+        "altText" => "カルーセル",
+        "template" => $return_message_text
+    ];
 
     //ポストデータ
     $post_data = [
