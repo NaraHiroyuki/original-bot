e3c20b6d21036796c9971928698ddf05992504dc
NaraHiroyuki <hiroyuki0818yakyu@icloud.com>
2019-07-08 22:41:52 +0900

    相対パスを修正

--- callback.php
+++ callback.php
@@ -1,8 +1,8 @@
 <?php
  $accessToken = 'Ak437zeTmxtWY3WLe6hMgOm++kaItQngv2RiKgwqPhb2B88FkStdcbMh55WhuRmf/hNTz+XlgyteKUfNhNl9PJQcxADRxqKd4laB+Tp9PR0s2vajpKF0ixE7eDx7sG+tdlnj+08JG0L5Pik94FVATAdB04t89/1O/w1cDnyilFU=';
 require_once("ID.php");
-require_once("date.php");
-require_once("site_date.php");
+require_once("date_data.php");
+require_once("site_data.php");
 //ユーザーからのメッセージ取得
 $json_string = file_get_contents('php://input');
 $json_object = json_decode($json_string);
