9a8aa2490c624945b40e481c947dfd07e991e22d
NaraHiroyuki <hiroyuki0818yakyu@icloud.com>
2019-07-08 22:37:11 +0900

    カルーセルを修正

--- callback.php
+++ callback.php
@@ -214,7 +214,7 @@ if($message_text == "福岡県の観光"){
                         [ 
                            'type' => 'uri', 
                            'label' => $message_text.'情報へ',
-                           'uri' => $date_data[$message_text]
+                           'uri' => $site_data[$message_text]
                         ] 
                    ] 
                ]
