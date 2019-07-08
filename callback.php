7e8210cfa1c0f419d5a823ba4215e74cb121d79d
NaraHiroyuki <hiroyuki0818yakyu@icloud.com>
2019-07-08 22:23:41 +0900

    カルーセルを修正

--- callback.php
+++ callback.php
@@ -222,35 +222,35 @@ if($message_text == "福岡県の観光"){
         ] 
    ];
    send_carousel($accessToken, $replyToken, $return_message_text );
-} elseif($message_text == "福岡県のデートスポット") {
-     
+} elseif($message_text == "福岡県のデートスポット"){
+    
     // カルーセルタイプ 
-     $return_message_text = [ 
-        'type' => 'template', 
-        'altText' => 'カルーセル', 
-        'template' => [
-             'type' => 'carousel', 
-             'columns' => [ 
-                [ 
-                    'title' => $message_text.'情報', 
-                    'text' => 'じゃらんの情報に移動します',
-                    'actions' => [ 
-                        [
-                            'type' => 'postback', 
-                            'label' => 'webhookにpost送信', 
-                            'data' => 'value' 
-                        ], 
+    $return_message_text = [ 
+       'type' => 'template', 
+       'altText' => 'カルーセル', 
+       'template' => [
+            'type' => 'carousel', 
+            'columns' => [ 
+               [ 
+                   'title' => $message_text.'情報', 
+                   'text' => 'じゃらんの情報に移動します',
+                    'actions' => [
+                       [
+                           'type' => 'postback', 
+                           'label' => 'webhookにpost送信', 
+                           'data' => 'value' 
+                       ], 
                         [ 
-                            'type' => 'uri', 
-                            'label' => $message_text.'情報へ', 
-                            'uri' => $date_data[$message_text]
-                        ]
+                           'type' => 'uri', 
+                           'label' => $message_text.'情報へ',
+                           'uri' => $site_date[$message_text]
+                        ] 
                    ] 
-               ] 
+               ]
            ] 
        ] 
-    ];
- 　　send_carousel($accessToken, $replyToken, $return_message_text );
+  ];
+  send_carousel($accessToken, $replyToken, $return_message_text );
 } else {
     //返信実行
     send_messages($accessToken, $replyToken, $message_type, $return_message_text);
